699064b957dc48eeed9dcff795f6b3e6e5f54340
KevinYuCode <76442800+KevinYuCode@users.noreply.github.com>
2022-08-30 18:27:54 -0400

    Fixed spelling mistakes

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -15,7 +15,7 @@ export const PROJECTS = [
   {
     name: "COOK HACK",
     description:
-      "Cook Hack is a recipe web app that simplifies the decision and planning process of cooking at home. The app implements computer vision to scan and identify ingredients from your camera and provides a list of recipes and step-by-step instructions for cooking those dish.",
+      "Cook Hack is a recipe web app that simplifies the decision and planning process of cooking at home. The app implements computer vision to scan and identify ingredients from your camera and provides a list of recipes and step-by-step instructions for cooking those dishes.",
     date: "Jan 28 – 30, 2022",
     ranking: "Winner of Hammer of the Gods Prize",
     image: COOK_HACK,
@@ -25,7 +25,7 @@ export const PROJECTS = [
   {
     name: "NOTE HACK",
     description:
-      "Note Hack is a note sharing web app thats sole purpose is to encourage others to share and collaborate together through digital note sharing. The app implments Firebase services allowing personalized student profiles to upload, update and delete notes they want to share with others.",
+      "Note Hack is a note-sharing web app that's sole purpose is to encourage others to share and collaborate together through digital note-sharing. The app implements Firebase services allowing personalized student profiles to upload, update and delete notes they want to share with others.",
     date: "Nov 6 – 7, 2021",
     ranking: "3rd Place Overall",
     image: NOTE_HACK,
