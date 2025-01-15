10f8b83600b2d3ad9f85246834ce5392a270cbcd
KevinYuCode <yu.kevin2002@gmail.com>
2025-01-15 17:23:30 -0500

    improvement(vite): convert cra to vite

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/index.css";
+import "./styles/customStyles.css";
+import "./styles/container.css";
+import "./styles/landingPage.css";
+import "./styles/nav.css";
+import "./styles/about.css";
+import "./styles/work.css";
+import "./styles/projects.css";
+import "./styles/contact.css";
+import "./styles/footer.css";
+import App from "./App";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc"; // or '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true,
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  // Additional config here if needed
+});
